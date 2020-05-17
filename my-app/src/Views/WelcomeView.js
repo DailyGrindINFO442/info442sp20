f075906f3d68acd69e7bd56af8a1ce801d8455e6
adamkpumputis <akyp@uw.edu>
2020-05-17 12:34:55 -0700

    Added live clock to Welcomeview

--- my-app/src/Views/WelcomeView.js
+++ my-app/src/Views/WelcomeView.js
@@ -4,19 +4,40 @@ export default class WelcomeView extends Component {
 
     // Pre-Conditions: WelcomeView is loaded
     // Post-Conditions: Displays current date and day of the week
-    displayDate() {
+    constructor(props) {
+        super(props);
+        this.state = {
+          date: new Date().toLocaleString()
+        };
+    }
+
+    componentDidMount() {
+        this.intervalID = setInterval(
+            () => this.tick(),
+            1000
+        );  
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalID);
+    }
+
+    tick() {
+        this.setState({
+            time: new Date().toLocaleString()
+        });
     }
 
     render() {
-        let today = new Date();
-
         return (
             <div className="welcome">
                 <div className="welcomeText">
                     <h2>Today is</h2>
-                    <h1>{today.toDateString()}</h1>
+                    <h1 className="Welcome-clock">
+                        {this.state.time}
+                    </h1>
                 </div>
             </div>
-        )
+        );
     }
 }
