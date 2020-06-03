4249daf5d7944f91a8c6cf5fe7d217d60fc552cb
magdanat <nathanmagdalera@gmail.com>
2020-06-03 01:21:15 -0700

    Updates

---
+++ precache-manifest.548684d1398085a2ad9e4d9367f97cc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44feec91496683433e0bffcf6102551e",
+    "url": "/info442sp20/index.html"
+  },
+  {
+    "revision": "2da05f6363e432781dba",
+    "url": "/info442sp20/static/css/2.2842baef.chunk.css"
+  },
+  {
+    "revision": "b56a9233f3e3a55c7586",
+    "url": "/info442sp20/static/css/main.9d0b356e.chunk.css"
+  },
+  {
+    "revision": "2da05f6363e432781dba",
+    "url": "/info442sp20/static/js/2.62a8c1d3.chunk.js"
+  },
+  {
+    "revision": "e9bc2d64a18f3da0440b678682e825d4",
+    "url": "/info442sp20/static/js/2.62a8c1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b56a9233f3e3a55c7586",
+    "url": "/info442sp20/static/js/main.22cb2d94.chunk.js"
+  },
+  {
+    "revision": "d9884eb997fab52e2b48",
+    "url": "/info442sp20/static/js/runtime-main.46d349e1.js"
+  }
+]);
